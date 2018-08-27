abec7e1ed59616d1eade44bbf9b9add6c3dea064
Dayo <admin@nerdroco.com>
2018-08-26 20:11:10 -0700

    Added Foreground... and CameraFollow, again
    Lost cameraFollow, had to reimplement it. Also added foreground.

---
+++ Assets/JetDroid/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    // public fields
+    public GameObject target;
+    public float scale = 4f;
+
+    // private field
+    private Transform t;
+
+    private void Awake()
+    {
+        // set camera size appropriately
+        var cam = GetComponent<Camera>();
+        cam.orthographicSize = (Screen.height / 2f) / scale;
+    }
+    // Use this for initialization
+    void Start () {
+        // set tranform to that of target object
+        t = target.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(target != null)
+        {
+            // update to follow transform of target with camera
+            transform.position = new Vector3(t.position.x, t.position.y, transform.position.z);
+        }
+	}
+}
