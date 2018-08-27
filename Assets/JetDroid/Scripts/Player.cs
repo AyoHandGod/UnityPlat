cfe7c961bf2a78b9f2b866531a2cd6c42c2500e1
Dayo <admin@nerdroco.com>
2018-08-26 17:25:36 -0700

    Left Right Movement for Player
    Add "Player" script. Allows Left/Right movement. Next, adding Jumping and animations

---
+++ Assets/JetDroid/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Public Fields
+    public float speed = 150f;
+    public Vector2 maxVelocity = new Vector2(60, 100);
+    public float airSpeedModifier = 0.3f;
+    public float jetSpeed = 200f;
+    public bool standing = true;
+    public float standingThreshold = 4.0f;
+
+
+    // Private Fields
+    private Rigidbody2D body2D;
+    private SpriteRenderer renderer2D;
+
+	// Use this for initialization
+	void Start () {
+        body2D = GetComponent<Rigidbody2D>();
+        renderer2D = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var absVelX = Mathf.Abs(body2D.velocity.x);
+        var forceX = 0f;
+        var forceY = 0f;
+
+        if (Input.GetKey("right"))
+        {
+            if(absVelX < maxVelocity.x)
+            {
+                forceX = standing ? speed : speed * airSpeedModifier;
+                renderer2D.flipX = false;
+            }
+        } else if (Input.GetKey("left"))
+        {
+            if(absVelX < maxVelocity.x)
+            {
+                forceX = standing ? -speed : -speed * airSpeedModifier;
+                renderer2D.flipX = true;
+            }
+        }
+
+        body2D.AddForce(new Vector2(forceX, forceY));
+	}
+}
