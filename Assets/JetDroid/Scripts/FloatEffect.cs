532b7b7eb956cc83ad75a7edc149dbe7b8dad81d
Dayo <admin@nerdroco.com>
2018-08-30 11:11:00 -0700

    Finalized Basics
    All Basic Elements add. Ready to be fleshed out for fun

---
+++ Assets/JetDroid/Scripts/FloatEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatEffect : MonoBehaviour {
+
+    // private fields
+    private float startY = 0f;
+    private float duration = 1f;
+    private RectTransform rectTrans;
+
+	// Use this for initialization
+	void Start () {
+        rectTrans = GetComponent<RectTransform>();
+        startY = rectTrans.anchoredPosition.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var newY = startY + (startY + Mathf.Cos(Time.time / duration * 2) / .1f);
+        rectTrans.anchoredPosition = new Vector2(rectTrans.position.x, newY);
+	}
+}
